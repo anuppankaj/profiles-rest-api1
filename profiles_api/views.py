4291eaedb72692095248ae753b3cdf697d12171e
Anup <anuppanka352@gmail.com>
2024-08-26 21:01:30 +0530

    added authentication and authoriation

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,9 +2,13 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
 
 
+
+from profiles_api import models
 from profiles_api import serializers
+from profiles_api import permission
 
 
 class HelloApiview(APIView):
@@ -98,6 +102,13 @@ class HelloViewSet(viewsets.ViewSet):
         """Handle removung an object"""
         return Response({'http_method': 'DELETE'})
     
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authenticatio_classes = (TokenAuthentication,)
+    permission_classes = (permission.UpdateOwnProfile,)
 
 
         
