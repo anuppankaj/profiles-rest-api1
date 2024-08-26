e37a7308a033e17ad7adb894ad5702c84ac4c4aa
Anup <anuppanka352@gmail.com>
2024-08-26 22:27:29 +0530

    added login api

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -4,6 +4,9 @@ from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+
 
 
 
@@ -112,6 +115,11 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     permission_classes = (permission.UpdateOwnProfile,)
     filter_backends = (filters.SearchFilter,)
     search_fiels = ('name', 'email',)
+
+class UserLoginApiView(ObtainAuthToken):
+    """Handle obtaing user authentication token"""
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
 
 
         
